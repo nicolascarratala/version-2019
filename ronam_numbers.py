62e3e012d34bf1f9b3579059965567f5b0fc04b4
martin98923 <martinrodriguito@gmail.com>
2019-03-26 16:24:58 -0300

    comit local

---
+++ ronam_numbers.py
@@ -0,0 +1,12 @@
+def roman_to_decimal(roman_number):
+    decimal_number=0
+    for letter in roman_number:
+        if letter=='I':
+            decimal_number=decimal_number+1
+        if letter=='V':
+            decimal_number=decimal_number+5
+        if letter=='X':
+            decimal_number=decimal_number+10
+        if letter=='L':
+            decimal_number=decimal_number+50
+    return decimal_number
