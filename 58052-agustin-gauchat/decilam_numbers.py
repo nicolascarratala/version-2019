373a038f764162bb0d56a0b4cc5a53864e7a7904
Agustin Gauchat <agus123456@hotmail.com>
2019-03-30 16:03:03 -0300

    DecimalV2

--- 58052-agustin-gauchat/decilam_numbers.py
+++ 58052-agustin-gauchat/decilam_numbers.py
@@ -1,13 +1,17 @@
-def decimal_to_roman(decimal_number, decimal_numberr):
+def decimal_to_roman(decimal_number):
     romann_number = ""
     ant = ""
+    
     for letter in decimal_number:
-        if decimal_numberr < 9:
+        if int(decimal_number) < 9:
             if letter == '1':
                 romann_number = romann_number + "I"
 
             if letter == '2':
                 romann_number = romann_number + "II"
+            
+            if letter == '3':
+                romann_number = romann_number + "III"
         
         ant = letter
 
