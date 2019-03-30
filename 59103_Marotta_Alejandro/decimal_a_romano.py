e8726e4d14549d9b0b43228a48da335fe41eafb5
amarotta1 <alitomarotta@gmail.com>
2019-03-30 19:15:04 -0300

    decimal a romano: cambie la logica, funciona hasta el 20 (tambien probe el 99)

--- 59103_Marotta_Alejandro/decimal_a_romano.py
+++ 59103_Marotta_Alejandro/decimal_a_romano.py
@@ -1,17 +1,69 @@
 def decimal_a_romano(decimal):
-    romano =''
+    ru =''
+    rd =''
+    rc =''
+    rm =''
+    unidad = 0
+    decena = 0
+    centena = 0
+    mil = 0
 
-
-    for letter in decimal:
-        if letter == '1':
-            romano = 'I'
-        if letter == '2':
-            romano = 'II'
-        if letter == '3':
-            romano = 'III'
-        if letter == '4':
-            romano = 'IV'
-        if letter == '5':
-            romano = 'V'
+    letras = str(decimal)
     
-    return romano
+   
+    
+    if len(letras)==1:
+        unidad = letras[0]
+   
+    if len(letras)==2:
+        decena = letras [0]
+        unidad = letras[1]
+     
+    
+
+    
+    if unidad == '1':
+        ru = 'I'
+    if unidad == '2':
+        ru = 'II'
+    if unidad == '3':
+        ru = 'III'
+    if unidad == '4':
+        ru = 'IV'
+    if unidad == '5':
+        ru = 'V'
+    if unidad == '6':
+        ru = 'VI'
+    if unidad == '7':
+        ru = 'VII'
+    if unidad == '8':
+        ru = 'VIII'
+    if unidad == '9':
+        ru = 'IX'
+    
+
+
+    if decena == '1':
+        rd = 'X'
+    if decena == '2':
+        rd = 'XX'
+    if decena == '3':
+        rd = 'XXX'
+    if decena == '4':
+        rd = 'XL'
+    if decena == '5':
+        rd = 'L'
+    if decena == '6':
+        rd = 'LX'
+    if decena == '7':
+        rd = 'LXX'
+    if decena == '8':
+        rd = 'LXXX'
+    if decena == '9':
+        rd = 'XC'
+
+        
+    return rd + ru
+    
+
+
--- 59103_Marotta_Alejandro/test_decimal_a_romano.py
+++ 59103_Marotta_Alejandro/test_decimal_a_romano.py
@@ -3,35 +3,75 @@ from decimal_a_romano import decimal_a_romano
 
 class TestDecimal(unittest.TestCase):
     def test_1 (self):
-        romanotest = decimal_a_romano('1')
+        romanotest = decimal_a_romano(1)
         self.assertEqual(romanotest, 'I')
 
 
     def test_2 (self):
-        romanotest = decimal_a_romano('2')
+        romanotest = decimal_a_romano(2)
         self.assertEqual(romanotest, 'II')
-        
     def test_3 (self):
-        romanotest = decimal_a_romano('3')
+        romanotest = decimal_a_romano(3)
         self.assertEqual(romanotest, 'III')
-
     def test_4 (self):
-        romanotest = decimal_a_romano('4')
+        romanotest = decimal_a_romano(4)
         self.assertEqual(romanotest, 'IV')
-
     def test_5 (self):
-        romanotest = decimal_a_romano('5')
+        romanotest = decimal_a_romano(5)
         self.assertEqual(romanotest, 'V')
+    def test_6 (self):
+        romanotest = decimal_a_romano(6)
+        self.assertEqual(romanotest, 'VI')
+    def test_7 (self):
+        romanotest = decimal_a_romano(7)
+        self.assertEqual(romanotest, 'VII')
+    def test_8 (self):
+        romanotest = decimal_a_romano(8)
+        self.assertEqual(romanotest, 'VIII')
+    def test_9 (self):
+        romanotest = decimal_a_romano(9)
+        self.assertEqual(romanotest, 'IX')
 
+    def test_10(self):
+        romanotest = decimal_a_romano(10)
+        self.assertEqual(romanotest, 'X')
 
+    def test_11 (self):
+        romanotest = decimal_a_romano(11)
+        self.assertEqual(romanotest, 'XI')
+    def test_12 (self):
+        romanotest = decimal_a_romano(12)
+        self.assertEqual(romanotest, 'XII')
+    def test_13 (self):
+        romanotest = decimal_a_romano(13)
+        self.assertEqual(romanotest, 'XIII')
+    def test_14 (self):
+        romanotest = decimal_a_romano(14)
+        self.assertEqual(romanotest, 'XIV')
+    def test_15 (self):
+        romanotest = decimal_a_romano(15)
+        self.assertEqual(romanotest, 'XV')
+    def test_16 (self):
+        romanotest = decimal_a_romano(16)
+        self.assertEqual(romanotest, 'XVI')
+    def test_17 (self):
+        romanotest = decimal_a_romano(17)
+        self.assertEqual(romanotest, 'XVII')
+    def test_18 (self):
+        romanotest = decimal_a_romano(18)
+        self.assertEqual(romanotest, 'XVIII')
+    def test_19 (self):
+        romanotest = decimal_a_romano(19)
+        self.assertEqual(romanotest, 'XIX')
+    def test_20 (self):
+        romanotest = decimal_a_romano(20)
+        self.assertEqual(romanotest, 'XX')
+    
 
-
-
-
-
-
-
-
+    def test_99 (self):
+        romanotest = decimal_a_romano(99)
+        self.assertEqual(romanotest, 'XCIX')
+    
 
 
 
