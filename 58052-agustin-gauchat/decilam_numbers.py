e59814ee326dcf1b25090f620569053c1aefda56
Agustin Gauchat <agus123456@hotmail.com>
2019-03-30 15:56:55 -0300

    DecimalV2

--- 58052-agustin-gauchat/decilam_numbers.py
+++ 58052-agustin-gauchat/decilam_numbers.py
@@ -1,2 +1,15 @@
-def decimal_to_roman(decimal_number):
-    return "I"
+def decimal_to_roman(decimal_number, decimal_numberr):
+    romann_number = ""
+    ant = ""
+    for letter in decimal_number:
+        if decimal_numberr < 9:
+            if letter == '1':
+                romann_number = romann_number + "I"
+
+            if letter == '2':
+                romann_number = romann_number + "II"
+        
+        ant = letter
+
+
+    return romann_number
--- 58052-agustin-gauchat/test_decimal_numbers.py
+++ 58052-agustin-gauchat/test_decimal_numbers.py
@@ -4,8 +4,12 @@ from decilam_numbers import decimal_to_roman
 class TestsDecimalNumbers(unittest.TestCase) :
 
     def test_decimal_1_to_roman(self):
-        roman_number = decimal_to_roman(1)
+        roman_number = decimal_to_roman('1', 1)
         self.assertEqual(roman_number, "I")
+
+    def test_decimal_2_to_roman(self):
+        roman_number = decimal_to_roman('2', 2)
+        self.assertEqual(roman_number, "II")
 
 
 if __name__ == '__main__':
