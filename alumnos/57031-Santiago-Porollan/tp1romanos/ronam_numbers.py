b9c1ed5028b6584e16bbc0945bb790294c4faf35
Santi Porollan <santiporollan@gmail.com>
2019-04-01 16:18:29 -0300

    decimal a romano

--- alumnos/57031-Santiago-Porollan/tp1romanos/ronam_numbers.py
+++ alumnos/57031-Santiago-Porollan/tp1romanos/ronam_numbers.py
@@ -45,5 +45,90 @@ def roman_to_decimal(roman_number):
                decimal_number=decimal_number-200       
 
       counter=counter+1
-            
+      
    return decimal_number
+
+def decimal_to_roman(decimal_num):
+   roman_number = ''
+   counter = 0
+   decimal_number = str(decimal_num)
+   for number in reversed(decimal_number):
+
+      if number == '1':
+         if counter == 0:
+            roman_number=roman_number+'I'
+         elif counter == 1:
+            roman_number='X'+roman_number
+         elif counter == 2:
+            roman_number='C'+roman_number
+         elif counter == 3:
+            roman_number='M'+roman_number
+
+      elif number == '2':
+         if counter==0:
+            roman_number = roman_number + 'II'
+         elif counter == 1:
+            roman_number='XX' + roman_number
+         elif counter == 2:
+            roman_number='CC'+ roman_number
+         elif counter == 3 :
+            roman_number ='MM' + roman_number
+
+      elif number == '3':
+         if counter==0:
+            roman_number = roman_number + 'III'
+         elif counter==1:
+            roman_number ='XXX' + roman_number
+         elif counter==2:
+            roman_number ='CCC' + roman_number
+
+      elif number == '4':
+         if counter==0:
+            roman_number = roman_number + 'IV'
+         elif counter==1:
+            roman_number = 'XL'+roman_number
+         elif counter==2:
+            roman_number = 'CD'+ roman_number
+
+      elif number == '5':
+         if counter==0:
+            roman_number = roman_number + 'V'
+         elif counter==1:
+            roman_number= 'L' +roman_number
+         elif counter==2:
+            roman_number= 'D' +roman_number
+
+      elif number == '6':
+         if counter==0:
+            roman_number = roman_number + 'VI'
+         elif counter==1:
+            roman_number= 'LX' + roman_number
+         elif counter== 2:
+            roman_number= 'DC' +roman_number
+
+      elif number == '7':
+         if counter==0:
+            roman_number = roman_number + 'VII'
+         elif counter== 1:
+            roman_number = 'LXX' + roman_number
+         elif counter == 2:
+            roman_number = 'DCC' + roman_number 
+      elif number == '8':
+         if counter==0:
+            roman_number = roman_number + 'VIII'
+         elif counter==1:
+            roman_number= 'LXXX'+roman_number
+         elif counter==2:
+            roman_number= 'DCCC'+roman_number
+      
+      elif number == '9':
+         if counter==0:
+            roman_number = roman_number + 'IX'
+         elif counter==1:
+            roman_number= 'XC'+roman_number
+         elif counter==2:
+            roman_number= 'CM'+roman_number
+
+      counter=counter+1
+  
+   return roman_number
--- alumnos/57031-Santiago-Porollan/tp1romanos/test_roman_numbers.py
+++ alumnos/57031-Santiago-Porollan/tp1romanos/test_roman_numbers.py
@@ -1,7 +1,9 @@
 import unittest
 
-from ronam_numbers import roman_to_decimal
-
+from ronam_numbers import (
+    roman_to_decimal,
+    decimal_to_roman,
+)
 
 
 
@@ -155,7 +157,71 @@ class TestRomanNumbers(unittest.TestCase):
     def test_roman_CMLIV_to_decimal(self):
 
         decimal_number = roman_to_decimal('CMLIV')
-        self.assertEqual(decimal_number,954)                     
+        self.assertEqual(decimal_number,954)                
+
+class TestDecimalToRomanNumbers(unittest.TestCase):
+    def test_decimal_1_to_roman(self):
+       roman_number = decimal_to_roman(1)
+       self.assertEqual(roman_number, 'I')
+    def test_decimal_4_to_roman(self):
+       roman_number = decimal_to_roman(4)
+       self.assertEqual(roman_number, 'IV')
+    def test_decimal_16_to_roman(self):
+       roman_number = decimal_to_roman(16)
+       self.assertEqual(roman_number, 'XVI')      
+    def test_decimal_10_to_roman(self):
+       roman_number = decimal_to_roman(10)
+       self.assertEqual(roman_number, 'X')
+    def test_decimal_14_to_roman(self):
+       roman_number = decimal_to_roman(14)
+       self.assertEqual(roman_number, 'XIV')    
+    def test_decimal_20_to_roman(self):
+       roman_number = decimal_to_roman(14)
+       self.assertEqual(roman_number, 'XIV')    
+    def test_decimal_24_to_roman(self):
+       roman_number = decimal_to_roman(24)
+       self.assertEqual(roman_number, 'XXIV')
+    def test_decimal_25_to_roman(self):
+       roman_number = decimal_to_roman(25)
+       self.assertEqual(roman_number, 'XXV')
+    def test_decimal_26_to_roman(self):
+       roman_number = decimal_to_roman(26)
+       self.assertEqual(roman_number, 'XXVI')
+    def test_decimal_47_to_roman(self):
+       roman_number = decimal_to_roman(47)
+       self.assertEqual(roman_number, 'XLVII')       
+    def test_decimal_59_to_roman(self):
+       roman_number = decimal_to_roman(59)
+       self.assertEqual(roman_number, 'LIX')  
+    def test_decimal_61_to_roman(self):
+       roman_number = decimal_to_roman(61)
+       self.assertEqual(roman_number, 'LXI')  
+    def test_decimal_77_to_roman(self):
+       roman_number = decimal_to_roman(77)
+       self.assertEqual(roman_number, 'LXXVII')  
+    def test_decimal_177_to_roman(self):
+       roman_number = decimal_to_roman(177)
+       self.assertEqual(roman_number, 'CLXXVII')  
+    def test_decimal_100_to_roman(self):
+       roman_number = decimal_to_roman(100)
+       self.assertEqual(roman_number, 'C')   
+    def test_decimal_101_to_roman(self):
+       roman_number = decimal_to_roman(101)
+       self.assertEqual(roman_number, 'CI')   
+    def test_decimal_120_to_roman(self):
+       roman_number = decimal_to_roman(120)
+       self.assertEqual(roman_number, 'CXX') 
+    def test_decimal_582_to_roman(self):
+       roman_number = decimal_to_roman(582)
+       self.assertEqual(roman_number, 'DLXXXII')    
+    def test_decimal_1000_to_roman(self):
+       roman_number = decimal_to_roman(1000)
+       self.assertEqual(roman_number, 'M')   
+
+
+
+
+
 if __name__ == '__main__':
 
    unittest.main()
