00430eec2250376f6243d3b88005545324dde545
Santi Porollan <santiporollan@gmail.com>
2019-04-07 18:09:12 -0300

    cambio en list_numbers.py

--- alumnos/57031-Santiago-Porollan/tp2/list_numbers.py
+++ alumnos/57031-Santiago-Porollan/tp2/list_numbers.py
@@ -1,10 +1,10 @@
 def find_max(list_of_numbers):
     if list_of_numbers:
         max=list_of_numbers[0]
+        for number in list_of_numbers:
+            if number>max:
+                max=number
+        return max
     else:
         return None
-    for number in list_of_numbers:
-        if number>max:
-            max=number
 
-    return max
