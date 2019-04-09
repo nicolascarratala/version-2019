7d95f95c989e79cca18cce93b8b360eb61fe9d98
Albertobarrio <albertobarrio2012@gmail.com>
2019-04-09 00:04:24 -0300

    test max number

---
+++ 56002-alberto-barrio/list_numbers.py
@@ -0,0 +1,6 @@
+def find_max(list_of_numbers):
+        max_number=0
+        for number in list_of_numbers:
+                if number > max_number:
+                        max_number=number
+        return max_number
