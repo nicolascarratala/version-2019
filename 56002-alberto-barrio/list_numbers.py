052259811df576960813f96ff0b35db6ce3a64da
Albertobarrio <albertobarrio2012@gmail.com>
2019-04-09 09:03:05 -0300

    Ejercicio 1 terminado

--- 56002-alberto-barrio/list_numbers.py
+++ 56002-alberto-barrio/list_numbers.py
@@ -1,9 +1,18 @@
 def find_max(list_of_numbers):
         max_number=0
+        """
+        #otra forma
         if list_of_numbers !=[] :
                 for number in list_of_numbers:
-                        if number > max_number :
+                        if number > max_number:
                                 max_number=number
         else:
-                max_number=None        
+                max_number=None
+        """
+        if not list_of_numbers:
+                max_number = None
+        else:
+                for number in list_of_numbers:
+                        if number > max_number:
+                                max_number=number          
         return max_number
