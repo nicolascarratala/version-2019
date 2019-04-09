77907f9286a957fe16f9fc962976ad1a8f278838
abcapo <agustinabelencapo@hotmail.com>
2019-04-09 19:37:50 -0300

    TP1

--- 58089-Agustina-Capo/ronam_numbers.py
+++ 58089-Agustina-Capo/ronam_numbers.py
@@ -10,12 +10,47 @@ def roman_to_decimal(roman_number):
             decimal-number= decimal-number + 1
         if letter == 'V':
             decimal-number= decimal-number + 5
+            if ant == 'I':
+                decimal_number = decimal_number - 2
         if letter == 'VI':
             decimal-number= decimal-number + 6
         if letter == 'VII':
             decimal-number= decimal-number + 7
         if letter == 'VIII':
             decimal-number= decimal-number + 8
+            if ant == 'I':
+                decimal_number = decimal_number - 1
         if letter == 'X':
             decimal-number= decimal-number + 10
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+        if letter == 'L':
+            decimal-number= decimal-number + 50
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+        if letter == 'C':
+            decimal-number= decimal-number + 100
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+        if letter == 'D':
+            decimal-number= decimal-number + 500
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+            if ant == 'C':
+                decimal_number = decimal_number - 200
+        if letter == 'M':
+            decimal-number= decimal-number + 1000
+            if ant == 'I':
+                decimal_number = decimal_number - 2
+            if ant == 'X':
+                decimal_number = decimal_number - 20
+            if ant == 'C':
+                decimal_number = decimal_number - 200
+        ant = letter
     return decimal-number
