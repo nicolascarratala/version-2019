b2f4229fa6a5a1eba66f157ad29da2129b55ce73
javicercasi <jacercasii@gmail.com>
2019-04-16 12:29:44 -0300

    comit local

--- clase03/58004-Cercasi/count_letters.py
+++ clase03/58004-Cercasi/count_letters.py
@@ -1,11 +1,11 @@
 def count_letters (palabra):
 
-diccionario={}
+    diccionario={}
 
-for letra in palabra:
-
-    if letra in diccionario:
-        diccionario[letra]+=1
-    else:
-        diccionario[letra]=1
-return diccionario
+    for letra in palabra:
+        if letra in diccionario:
+            diccionario[letra]+=1
+        else:
+            diccionario[letra]=1
+    
+    return diccionario
